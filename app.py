77f5fedb9800ac65903de9f9d1b8b8ddacb59192
Leonardo Martins <leonardomartins.ads@gmail.com>
2024-12-17 23:30:01 +0000

    Remove Bug

--- app.py
+++ app.py
@@ -8,12 +8,3 @@ csrf = CSRFProtect(app)
 @app.route("/")
 def pagina_inicial():
     return "Leonardo Martins v1.0"
-
-@app.route('/bug')
-def bad():
-    try:
-        raise TypeError()
-    except TypeError as e:
-        print(e)
-    except TypeError as e:
-        print("Duplicado, ou seja, nunca vai entrar aqui.")
