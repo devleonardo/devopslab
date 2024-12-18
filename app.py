5a5c71679cb7b4037e3239276891a989b1f0f2e2
Leonardo Martins <leonardomartins.ads@gmail.com>
2024-12-17 22:36:38 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,8 @@ from flask import Flask
 from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
-
 csrf = CSRFProtect(app)
 
 @app.route("/")
 def pagina_inicial():
-    return "Leonardo Martins v1.0"
+    return "Leonardo Martins v1.0 - Lab Concluído"
